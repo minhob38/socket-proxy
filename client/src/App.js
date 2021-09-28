50ee7778d3ed2da7ffc20bb1790f911ce1a0a22a
kwon, minho <minhob38@gmail.com>
2021-09-28 20:48:55 +0900

    implement socket client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import socketio from "socket.io-client";
+
 function App() {
-  return <div>hello</div>;
+  const [socket, setSocket] = useState(null);
+
+  return (
+    <>
+      {socket && <div>connection state</div>}
+      <button
+        onClick={() => {
+          if (socket) socket.disconnect();
+
+          const client = socketio("http://localhost:3001");
+          setSocket(client);
+          client.on("send", () => console.log("confirmed server received client's data"));
+        }}
+      >
+        connect
+      </button>
+      <button
+        onClick={() => {
+          socket.emit("send", "my data : )");
+        }}
+      >
+        send
+      </button>
+    </>
+  );
 }
 
 export default App;
