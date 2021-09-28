83d144c7f088e35f09838748b78f0a68edeff5a5
kwon, minho <minhob38@gmail.com>
2021-09-28 20:03:55 +0900

    Implement socket server

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,27 @@
 const Koa = require("koa");
 const Router = require("@koa/router");
 const http = require("http");
+const socketio = require("socket.io");
 
 const app = new Koa();
 const router = new Router();
 
+const server = http.createServer(app.callback());
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  socket.on("send", (data) => {
+    console.log("i recieved data");
+    socket.emit("send", "data from server : )");
+  });
+
+  socket.on("disconnect", (data) => {
+    console.log("client socket disconnected");
+  });
+});
+
 router.get("/", (ctx) => (ctx.body = "hello"));
 
 app.use(router.routes());
-
-const server = http.createServer(app.callback());
 
 module.exports = server;
