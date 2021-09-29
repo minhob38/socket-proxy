cdc5abbefce6bd64da21341d91b6fbd570eaa5a5
kwon, minho <minhob38@gmail.com>
2021-09-29 15:40:55 +0900

    add proxy

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,43 @@ import socketio from "socket.io-client";
 
 function App() {
   const [socket, setSocket] = useState(null);
+  const [target, setTarget] = useState("http://localhost:3001");
 
   return (
     <>
-      {socket && <div>connection state</div>}
-      <button
-        onClick={() => {
-          if (socket) socket.disconnect();
+      socket connection target
+      <div>
+        <input onChange={(ev) => setTarget(ev.value)} value={target} />
+        {socket && <div>connection state</div>}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (socket) socket.disconnect();
 
-          const client = socketio("http://localhost:3001/api");
-          setSocket(client);
-          client.on("send", () => console.log("confirmed server received client's data"));
-        }}
-      >
-        connect
-      </button>
+            const client = socketio(`${target}/api`);
+            setSocket(client);
+            client.on("send", () => console.log("confirmed server received client's data"));
+          }}
+        >
+          connect
+        </button>
+        <button
+          onClick={() => {
+            socket.emit("send", "my data : )");
+          }}
+        >
+          socket send
+        </button>
+      </div>
       <button
-        onClick={() => {
-          socket.emit("send", "my data : )");
+        onClick={async () => {
+          const response = await fetch("/api/home");
+          const _response = await response;
+          console.log(_response);
         }}
       >
-        send
+        rest request
       </button>
     </>
   );
