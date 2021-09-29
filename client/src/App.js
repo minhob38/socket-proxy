964a89f4ed1a23603affa88ef787f8a8e4bd88eb
kwon, minho <minhob38@gmail.com>
2021-09-29 18:30:10 +0900

    design front

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,31 @@ import socketio from "socket.io-client";
 
 function App() {
   const [socket, setSocket] = useState(null);
-  const [target, setTarget] = useState("http://localhost:3001");
+  const [serverAddr, setServerAddr] = useState("http://localhost:3001");
+  const [reqAddr, setReqAddr] = useState("/api/home");
 
   return (
     <>
-      socket connection target
-      <div>
-        <input onChange={(ev) => setTarget(ev.value)} value={target} />
-        {socket && <div>connection state</div>}
-      </div>
+      <h4>server address</h4>
+      <input onChange={(ev) => setServerAddr(ev.target.value)} value={serverAddr} />
+      <hr />
+      <div>localhost: http://localhost:3001</div>
+      <div>without nginx: http://34.64.177.34:3001</div>
+      <div>with nginx: http://34.64.177.34:8080</div>
+      <hr />
+      <h4>socket test</h4>
+      {socket ? <div>connection : )</div> : <div>disconnection : (</div>}
       <div>
         <button
           onClick={() => {
             if (socket) socket.disconnect();
 
-            const client = socketio(`${target}/api`);
+            const client = socketio(`${serverAddr}/api`);
             setSocket(client);
-            client.on("send", () => console.log("confirmed server received client's data"));
+            client.on("send", (data) => {
+              console.log(data);
+              // console.log("confirmed server received client's data");
+            });
           }}
         >
           connect
@@ -32,14 +40,28 @@ function App() {
           socket send
         </button>
       </div>
+      <hr />
+      <h4>rest api test</h4>
+      <div>
+        <input onChange={(ev) => setReqAddr(ev.target.value)} value={reqAddr} />
+      </div>
       <button
         onClick={async () => {
-          const response = await fetch("/api/home");
+          const response = await fetch(`${serverAddr}${reqAddr}`);
           const _response = await response;
           console.log(_response);
         }}
       >
-        rest request
+        request without react proxy
+      </button>
+      <button
+        onClick={async () => {
+          const response = await fetch(`${reqAddr}`);
+          const _response = await response;
+          console.log(_response);
+        }}
+      >
+        request with react proxy
       </button>
     </>
   );
